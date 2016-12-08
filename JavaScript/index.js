bec29e34fbaf8a4e60e0ab3bf4292106cfca67de
万林 <chinabluesky335@gmail.com>
2016-12-09 01:36:33 +0800

    主页搭建

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,8 +1,21 @@
 $(function(){
-	$(".menuDiv").mouseenter(function(){
-		$(this).stop().animate({width:'100px'},250);
-	}).mouseleave(function(){
-		$(this).stop().animate({width:'40px'},250);
-	});
-})
+	resizeBgImage();
+});
 
+$(window).resize(function(){
+	resizeBgImage();
+}); 
+
+function resizeBgImage(){
+	var windowWidth = $(window).width();
+	var windowHeight = $(window).height();
+	
+	if(windowHeight/windowWidth<9/16){
+		var imagepositiony = (windowWidth * (9/16)-windowHeight)/2*-1;
+		$(".bgdiv").css({"background-size":"100% auto","background-position-y":imagepositiony,"background-position-x":0});
+	}else{
+		var imagepositionx = (windowHeight * (16/9)-windowWidth)/2*-1;
+		$(".bgdiv").css({"background-size":"auto 100%","background-position-x":imagepositionx,"background-position-y":0});
+	}
+	
+}
